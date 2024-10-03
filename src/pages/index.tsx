ff139c0b66be240e7e504f3b3b09b12a47dc111d
supra <rikc.2000@gmail.com>
2024-10-03 19:49:39 -0400

    minor

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,14 +2,12 @@
 // import Image from 'next/image'
 // import { Inter } from 'next/font/google'
 // import styles from '@/styles/Home.module.css'
-import {
-  default as Profile,
-  default as ProfileMistake,
-} from "./components/profile";
-import { default as ProfileProps } from "./components/profile_props";
+import Profile from "./components/profile";
+import ProfileProps from "./components/profile_props";
 import Bio from "./qcomps/bios";
 import MyFirstComp from "./qcomps/firstcomp";
 import GalleryProps from "./qcomps/gallery_props";
+import ProfileMistake from "./qcomps/profile_mistake";
 import PackingList from "./qcomps/props_item";
 import RecipeList from "./qcomps/recipes";
 import TodoList from "./qcomps/todos";
