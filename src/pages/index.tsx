2e2271f7dacac173cff836f2ee004a088c4cf8cc
supra <rikc.2000@gmail.com>
2024-10-03 23:02:44 -0400

    shopping cart component fixed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,24 +2,16 @@
 // import Image from 'next/image'
 // import { Inter } from 'next/font/google'
 // import styles from '@/styles/Home.module.css'
-import Profile from "./components/profile";
-import ProfileProps from "./components/profile_props";
-import Bio from "./qcomps/bios";
-import MyFirstComp from "./qcomps/firstcomp";
-import GalleryProps from "./qcomps/gallery_props";
-import ProfileMistake from "./qcomps/profile_mistake";
-import PackingList from "./qcomps/props_item";
-import RecipeList from "./qcomps/recipes";
 import RequestTracker from "./qcomps/shoppingCart";
 import Gallery from "./qcomps/state";
 import Form from "./qcomps/stuckForm";
 import FeedbackForm from "./qcomps/thankYouCrash";
-import TodoList from "./qcomps/todos";
 
 export default function Home() {
   return (
     <>
-      <Profile />
+      {/* <h1>React Components</h1> */}
+      {/* <Profile />
       <ProfileMistake />
       <MyFirstComp />
       <Bio />
@@ -27,23 +19,25 @@ export default function Home() {
       <ProfileProps />
       <GalleryProps />
       <PackingList />
-      <RecipeList />
+      <RecipeList /> */}
 
       {/* LAB: React Update State */}
       <hr />
       <h1>React Update State</h1>
 
       <Gallery />
-      <br />
+      <hr />
 
       <Form />
-      <br />
+      <hr />
 
       <FeedbackForm />
-      <br />
+      <hr />
+
+      <hr />
 
       <RequestTracker />
-      <br />
+      <hr />
     </>
   );
 }
