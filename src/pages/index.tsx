630d29e6c428e1e9061f85af4d494507ea5db1d2
supra <rikc.2000@gmail.com>
2024-10-04 19:20:00 -0400

    lab changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,28 @@
 // import { Inter } from 'next/font/google'
 // import styles from '@/styles/Home.module.css'
 
+import List from "./qcomps/list_keys_id";
+import RecipeList from "./qcomps/recipes";
+import Gallery from "./qcomps/state";
+import Form from "./qcomps/stuckForm";
+import FeedbackForm from "./qcomps/thankYouCrash";
+
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <List />
+      <hr />
+
+      <RecipeList />
+      <hr />
+
+      <Gallery />
+      <hr />
+
+      <Form />
+      <hr />
+
+      <FeedbackForm />
+    </>
+  );
 }
